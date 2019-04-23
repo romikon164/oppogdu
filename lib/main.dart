0d882f2be34fdd9429498a6c5acdd8d1c76808ca
Roman Byzov <romikon164@gmail.com>
2019-04-23 14:38:46 +0400

    Eshe odin commit5

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,67 @@ import 'package:oppo_gdu/src/support/auth/service.dart';
 import 'package:oppo_gdu/src/presenters/news/news_list_presenter.dart';
 import 'package:oppo_gdu/src/data/database/service.dart';
 import 'package:oppo_gdu/src/data/database/providers/news.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() => runApp(Application());
 
 class Application extends StatelessWidget
 {
+    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+    Application({Key key}): super(key: key) {
+        _firebaseInitialize();
+        _databaseInitialize();
+        _apiInitialize();
+    }
+
     // This widget is the root of your application.
     @override
     Widget build(BuildContext context)
+    {
+        Router router = Router();
+
+        return MaterialApp(
+            home: NewsListPresenter(router).view as StatefulWidget,
+            navigatorKey: router.navigatorKey,
+            theme: ThemeBuilder().build(),
+        );
+    }
+
+    void _firebaseInitialize()
+    {
+        _firebaseMessaging.configure(
+            onMessage: (Map<String, dynamic> message) async {
+                print("onMessage: $message");
+            },
+            onLaunch: (Map<String, dynamic> message) async {
+                print("onLaunch: $message");
+            },
+            onResume: (Map<String, dynamic> message) async {
+                print("onResume: $message");
+            },
+        );
+
+        _firebaseMessaging.requestNotificationPermissions(
+            const IosNotificationSettings(
+                sound: true,
+                badge: true,
+                alert: true
+            )
+        );
+
+        _firebaseMessaging.onIosSettingsRegistered
+            .listen((IosNotificationSettings settings) {
+                print("Settings registered: $settings");
+            });
+
+        _firebaseMessaging.getToken().then((String token) {
+            assert(token != null);
+            print("token: $token");
+        });
+    }
+
+    void _databaseInitialize()
     {
         try {
             DatabaseService.instance.addDatabaseProvider(
@@ -22,7 +75,10 @@ class Application extends StatelessWidget
         } catch (e) {
 
         }
+    }
 
+    void _apiInitialize()
+    {
         ApiService.buildInstance(
             baseUrl: "http://api.oppo-gdu.ru",
             clientId: 2,
@@ -30,13 +86,5 @@ class Application extends StatelessWidget
         );
 
         AuthService.instance.initService();
-
-        Router router = Router();
-
-        return MaterialApp(
-            home: NewsListPresenter(router).view as StatefulWidget,
-            navigatorKey: router.navigatorKey,
-            theme: ThemeBuilder().build(),
-        );
     }
 }
